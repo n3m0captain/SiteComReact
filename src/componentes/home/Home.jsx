8bbbc5904c974e7c1d3685aae744575305457876
Nemo <contato@n3m0captain.org>
2020-09-25 14:30:31 -0300

    Update Home.jsx

--- src/componentes/home/Home.jsx
+++ src/componentes/home/Home.jsx
@@ -6,7 +6,7 @@ export default props =>
 <div className= 'display-4'>
     Bem Vindo!!!
     <hr/>
-      <a href="https://github.com/n3m0captain/SiteComReact/blob/master/src/assets/imgs/IMG_20181025_163710251.jpg?raw=true">Nemo Dev do n3m0captain</a>
+      <a href="https://github.com/n3m0captain">Nemo Dev do n3m0captain</a>
     <hr/>
         
     <iframe width="400" height="600"src="https://github.com/n3m0captain"></iframe> 
